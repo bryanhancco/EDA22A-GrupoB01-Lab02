d186fcf480b883980de876148894830789fdb398
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-13 20:29:16 -0500

    agrego el metodo desplazar a la izquierda y pruebo en el main

--- mainAPP.java
+++ mainAPP.java
@@ -2,12 +2,14 @@ public class mainAPP {
     public static void main(String[] args) {
       //aqui probarè los mètodos
       ejercicio1 aEjercicio1 = new ejercicio1();
+      ejercicio2 aEjercicio2 = new ejercicio2();
       int arr[] = { 0, 1, 2, 3, 4, 5 };
       
       aEjercicio1.invertirArray(arr);
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(aEjercicio1.invertirArray(arr)[i]);
+            System.out.println(aEjercicio2.rotarIzquierdaArray(arr)[i]);
         }
+        
 
 
 
