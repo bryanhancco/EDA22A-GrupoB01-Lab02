7de4f9081367745819866c766b8de5fbed7f5c6a
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-13 20:05:26 -0500

    agrego la plantilla del ejercicio 2

---
+++ ejercicio2.java
@@ -0,0 +1,8 @@
+public class ejercicio2 {
+    public int[] rotarIzquierdaArray(int[] A){
+        /** */
+        //Procedimiento para rotar la matriz
+        /** */
+        return Aiz;
+    }
+}
