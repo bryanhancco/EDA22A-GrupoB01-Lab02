3e3101b1b2428e9493e0caa867f3b95af977927d
francoCarMar <fcardenasm@unsa.edu.pe>
2022-05-14 16:42:14 -0500

    completo y corrijo los saltos de lìnea y el espaciado del print

--- ejercicio3.java
+++ ejercicio3.java
@@ -1,9 +1,13 @@
 public class ejercicio3 {
     public void trianguloRecursivo(int base){
         if(base == 1){
-            System.out.println("*\n");
+            System.out.println("*");
         } else {
             trianguloRecursivo(base -1);
+            for (int i = 0; i < base; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
