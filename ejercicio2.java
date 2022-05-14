a5e0b818f809daa208e1c336a409c479c5a836e5
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-13 20:08:09 -0500

    complemento el ejercicio2, si la resta de i-d es mayor = 0 no hay problema en establecer la nueva posiciòn

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,8 +1,12 @@
 public class ejercicio2 {
     public int[] rotarIzquierdaArray(int[] A){
-        /** */
-        //Procedimiento para rotar la matriz
-        /** */
+        int[] Aiz = new int[A.length];
+        int d = 2, aux;
+        for (int i = 0; i < Aiz.length; i++) {
+            aux = A[i];
+            if (i - d >= 0)
+                Aiz[i - d] = aux;
+        }
         return Aiz;
     }
 }
