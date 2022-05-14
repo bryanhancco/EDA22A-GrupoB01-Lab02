de2b05cb48623eb790ee6900008d1dcb0c2c9945
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-13 19:57:47 -0500

    creo el main con un array

--- mainAPP.java
+++ mainAPP.java
@@ -1,6 +1,11 @@
 public class mainAPP {
     public static void main(String[] args) {
       //aqui probarè los mètodos
+      ejercicio1 aEjercicio1 = new ejercicio1();
+        int tuviejita[] = { 0, 1, 2, 3, 4, 5 };
+
+        
+
 
     }
 }
