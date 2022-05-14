057b680287e33cd0443c725959a9af611c9d9611
Barbara280801 <bcarrillo@unsa.edu.pe>
2022-05-13 19:58:31 -0500

    creo la funcion en la clase ejercicio1  solo utilizo un for para invertir los dato

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,8 +1,9 @@
 public class ejercicio1 {
     public int[] invertirArray(int[] A){
-        /** */
-        //Procedimiento para invertir la matriz
-        /** */
+        int[] Ain = new int[A.length];
+        for (int i = Ain.length - 1, j = 0; i >= 0; i--, j++) {
+            Ain[j] = A[i];
+        }
         return Ain;
     }
 }
